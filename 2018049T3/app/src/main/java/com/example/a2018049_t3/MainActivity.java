3259d2c9a48527d904142e17a06211f2308462cb
Agusti Bayu <Agustibayu.ABS@gmail.com>
2022-10-25 11:43:15 +0700

    1# Onclick menu

--- 2018049T3/app/src/main/java/com/example/a2018049_t3/MainActivity.java
+++ 2018049T3/app/src/main/java/com/example/a2018049_t3/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.a2018049_t3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayout item1 = findViewById(R.id.item1);
+        LinearLayout item2 = findViewById(R.id.item2);
+        LinearLayout item3 = findViewById(R.id.item3);
+
+        item1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), MainActivity2.class);
+                startActivity(i);
+            }
+        });
+        item2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), MainActivity3.class);
+                startActivity(i);
+            }
+        });
+        item3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), MainActivity4.class);
+                startActivity(i);
+            }
+        });
     }
+
 }
