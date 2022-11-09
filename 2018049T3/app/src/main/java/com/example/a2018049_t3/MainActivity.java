34f533cb9144f2b54c63d9c6217d464abc0ea502
Agusti Bayu <Agustibayu.ABS@gmail.com>
2022-11-09 19:50:59 +0700

    5# Membuat  RecyclerView

--- 2018049T3/app/src/main/java/com/example/a2018049_t3/MainActivity.java
+++ 2018049T3/app/src/main/java/com/example/a2018049_t3/MainActivity.java
@@ -1,44 +1,70 @@
 package com.example.a2018049_t3;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.LinearLayout;
+import android.util.Log;
+import android.view.MenuItem;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.navigation.NavigationView;
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle abdt;
+    RecyclerView recylerView;
+    String s1[], s2[],s3[];
+    int images[] =
+            {R.drawable.kebab,R.drawable.risoles,R.drawable.jus,R.drawable.piesusu};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recylerView = findViewById(R.id.recyleviewJajan);
+        s1 = getResources().getStringArray(R.array.makanan);
+        s2 = getResources().getStringArray(R.array.deskripsi);
+        s3 = getResources().getStringArray(R.array.star);
+        MakananAdapter appAdapter = new MakananAdapter(this,s1,s2,s3,images);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
+        recylerView.setLayoutManager(linearLayoutManager);
+        recylerView.setAdapter(appAdapter);
 
-        LinearLayout item1 = findViewById(R.id.item1);
-        LinearLayout item2 = findViewById(R.id.item2);
-        LinearLayout item3 = findViewById(R.id.item3);
+        dl = findViewById(R.id.drawer_layout);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        abdt = new ActionBarDrawerToggle(this,dl,R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        abdt.setDrawerIndicatorEnabled(true);
+        dl.addDrawerListener(abdt);
 
-        item1.setOnClickListener(new View.OnClickListener() {
+        abdt.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationView nav_view = (NavigationView)findViewById(R.id.nav_view);
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(), MainActivity2.class);
-                startActivity(i);
-            }
-        });
-        item2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(), MainActivity3.class);
-                startActivity(i);
-            }
-        });
-        item3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(), MainActivity4.class);
-                startActivity(i);
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.nav_message){
+                    Intent a = new Intent(MainActivity.this, MainActivity.class);
+                    startActivity(a);
+                }else if (id == R.id.nav_chat){
+                    Intent a = new Intent(MainActivity.this, MainActivity2.class);
+                    startActivity(a);
+                }else if (id == R.id.nav_profile){
+                    Intent a = new Intent(MainActivity.this, MainActivity3.class);
+                    startActivity(a);
+                }else if (id == R.id.nav_profile){
+                    Intent a = new Intent(MainActivity.this, MainActivity4.class);
+                    startActivity(a);
+                }
+                return true;
             }
         });
     }
-
 }
--- 2018049T3/app/src/main/java/com/example/a2018049_t3/MainActivity3.java
+++ 2018049T3/app/src/main/java/com/example/a2018049_t3/MainActivity3.java
@@ -1,11 +1,17 @@
 package com.example.a2018049_t3;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import android.content.Intent;
 
 import android.os.Bundle;
-
+import android.view.MenuItem;
+import com.google.android.material.navigation.NavigationView;
 public class MainActivity3 extends AppCompatActivity {
-
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle abdt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
