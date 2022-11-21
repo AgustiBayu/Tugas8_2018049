ebe5b429b8f497cfb3871fb9837b2419dc59e75b
Agusti Bayu <Agustibayu.ABS@gmail.com>
2022-11-22 00:08:09 +0700

    6# Alarm Manager

--- 2018049T3/app/src/main/java/com/example/a2018049_t3/MainActivity.java
+++ 2018049T3/app/src/main/java/com/example/a2018049_t3/MainActivity.java
@@ -5,22 +5,30 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.a2018049_t3.databinding.ActivityMainBinding;
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.timepicker.MaterialTimePicker;
+
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
     private DrawerLayout dl;
     private ActionBarDrawerToggle abdt;
+
     RecyclerView recylerView;
-    String s1[], s2[],s3[];
+    String s1[], s2[], s3[];
     int images[] =
-            {R.drawable.kebab,R.drawable.risoles,R.drawable.jus,R.drawable.piesusu};
+            {R.drawable.kebab, R.drawable.risoles, R.drawable.jus, R.drawable.piesusu};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,36 +38,37 @@ public class MainActivity extends AppCompatActivity {
         s1 = getResources().getStringArray(R.array.makanan);
         s2 = getResources().getStringArray(R.array.deskripsi);
         s3 = getResources().getStringArray(R.array.star);
-        MakananAdapter appAdapter = new MakananAdapter(this,s1,s2,s3,images);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
+        MakananAdapter appAdapter = new MakananAdapter(this, s1, s2, s3, images);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         recylerView.setLayoutManager(linearLayoutManager);
         recylerView.setAdapter(appAdapter);
 
         dl = findViewById(R.id.drawer_layout);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        abdt = new ActionBarDrawerToggle(this,dl,R.string.navigation_drawer_open,
+        abdt = new ActionBarDrawerToggle(this, dl, R.string.navigation_drawer_open,
                 R.string.navigation_drawer_close);
         abdt.setDrawerIndicatorEnabled(true);
         dl.addDrawerListener(abdt);
 
         abdt.syncState();
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        NavigationView nav_view = (NavigationView)findViewById(R.id.nav_view);
+        NavigationView nav_view = (NavigationView) findViewById(R.id.nav_view);
         nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 int id = item.getItemId();
-                if (id == R.id.nav_message){
+                if (id == R.id.nav_message) {
                     Intent a = new Intent(MainActivity.this, MainActivity.class);
                     startActivity(a);
-                }else if (id == R.id.nav_chat){
+                } else if (id == R.id.nav_chat) {
                     Intent a = new Intent(MainActivity.this, MainActivity2.class);
                     startActivity(a);
-                }else if (id == R.id.nav_profile){
+                } else if (id == R.id.nav_profile) {
                     Intent a = new Intent(MainActivity.this, MainActivity3.class);
                     startActivity(a);
-                }else if (id == R.id.nav_profile){
+                } else if (id == R.id.nav_profile) {
                     Intent a = new Intent(MainActivity.this, MainActivity4.class);
                     startActivity(a);
                 }
